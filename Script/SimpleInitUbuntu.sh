c78110ff985cccf21ee9481f4cde0783c873516b
qianfei11 <490584635@qq.com>
2018-10-24 15:44:37 +0800

    2018/10/24

--- Script/SimpleInitUbuntu.sh
+++ Script/SimpleInitUbuntu.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install vim
 sudo apt-get install git
+sudo apt-get install zsh
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 source ~/.zshrc
