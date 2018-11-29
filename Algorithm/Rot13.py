2162ff1ab388ec4dc59fc3ff76b2d1c5940a477e
qianfei11 <490584635@qq.com>
2018-11-29 14:42:53 +0800

    2018/11/29

--- Algorithm/Rot13.py
+++ Algorithm/Rot13.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python
-
+import string
 s = input()
-s1 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-s2 = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
-l1 = list(s1)
-l2 = list(s2)
-d = dict(zip(l1, l2))
-l1 = []
-for i in s:
-    if i in d:
-        data = d.get(i)
-    else:
-        data = i
-    l1.append(data)
-
-l2 = ['']
-for i in l1:
-    l2[0] = l2[0] + i
-
-print(l2[0])
+rot13 = string.maketrans( 
+    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
+    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
+    )
+s = string.translate(s, rot13)
+print s
