39d694b9e9430a1f335a91f7b7924ab8013dace5
assassinq <490584635@qq.com>
2019-02-27 23:16:16 +0800

    2019/2/27

--- Algorithm/Rot13.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-import string
-s = input()
-rot13 = string.maketrans( 
-    "ABCDEFGHIJKLMabcdefghijklmNOPQRSTUVWXYZnopqrstuvwxyz", 
-    "NOPQRSTUVWXYZnopqrstuvwxyzABCDEFGHIJKLMabcdefghijklm"
-    )
-s = string.translate(s, rot13)
-print s
